2116a3e3e0df3c0cf8ed544d4fe0411fbaa8abf3
everestp <paudeleverest09@gmail.com>
2026-01-09 17:24:39 +0545

    Add http2 and https to API

---
+++ http-server/go.mod
@@ -0,0 +1,7 @@
+module server
+
+go 1.24.11
+
+require golang.org/x/net v0.48.0
+
+require golang.org/x/text v0.32.0 // indirect
--- http-server/server.go
+++ http-server/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/http2"
 )
 
 
@@ -11,11 +14,38 @@ func main(){
 	http.HandleFunc("/",  func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintln(w,"Hello server")
 	})
-	const port string = ":3000"
-	fmt.Println("Server is listening om port :",port)
-	err :=http.ListenAndServe(port, nil)
-	if err != nil{
-		log.Fatalln("error starting the server",err)
+		http.HandleFunc("/orders",  func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintln(w,"Handling orders")
+	})
+	const port string = "3000"
+	//Load the tls certificate
+	cert :="cert.pem"
+	key :="key.pem"
+
+	//Config the tls
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
 	}
+
+	//Create a customer server
+	server := &http.Server{
+		Addr: fmt.Sprintf(":%v",port),
+		Handler: nil,
+		TLSConfig: tlsConfig,
+	}
+
+	//Enable http2
+	http2.ConfigureServer(server,&http2.Server{})
+	fmt.Println("Server is listening om port :",port)
+    err := server.ListenAndServeTLS(cert, key)
+   if err != nil{
+	 	log.Fatalln("error starting the server",err)
+	 }
+// This is the HTTP 1.1 Server with TLS
+
+	// err :=http.ListenAndServe(port, nil)
+	// if err != nil{
+	// 	log.Fatalln("error starting the server",err)
+	// }
 	
 }
